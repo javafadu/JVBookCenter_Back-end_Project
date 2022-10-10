fba03789dd48ae05085bd0596f77d1853f4949ec
alkeskn85 <alkeskn85@gmail.com>
2022-10-10 16:23:33 +0200

    V2

--- src/main/java/com/library/domain/Category.java
+++ src/main/java/com/library/domain/Category.java
@@ -3,8 +3,7 @@ package com.library.domain;
 import lombok.*;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+
 
 @Getter
 @Setter
@@ -14,25 +13,25 @@ import javax.validation.constraints.Size;
 
 @Table(name = "tbl_categories")
 @Entity
-public class Categories {
+public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private Long id;
 
-    @Size(min = 2, max = 80, message = "Your Name '${validatedValue}' must be between {min} and {max} chars long")
-    @NotNull(message = "Please provide your name")
+
     @Column(nullable = false, length = 80)
     private String name;
 
 
     @Column(nullable = false)
-    private boolean builtin=false;
+    private Boolean builtin=false;
 
 
 
     @Column(nullable = false)
-    private int sequence;
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer sequence;
 
 }
