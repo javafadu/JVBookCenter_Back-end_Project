4281e07a2accca6b5fc8a19db140acfd3ec19281
javafadu <javafadu@gmail.com>
2022-10-10 17:45:21 +0300

    v2

--- src/main/java/com/library/domain/Publisher.java
+++ src/main/java/com/library/domain/Publisher.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 import javax.validation.constraints.Size;
 
 @Entity
-@Table(name = "tbl_publisher")
+@Table(name = "tbl_publishers")
 @Getter
 @Setter
 @AllArgsConstructor
@@ -20,10 +20,9 @@ public class Publisher {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(nullable = false)
-    @Size(min = 2, max = 50)
+    @Column(length = 50, nullable = false)
     private String name;
 
-    @Column(nullable = false, columnDefinition="tinyint(1) default 1")
-    Boolean builtIn;
+    @Column(nullable = false)
+    Boolean builtIn =false;
 }
