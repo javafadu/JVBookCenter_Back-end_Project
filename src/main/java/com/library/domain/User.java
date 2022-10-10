b7af1405258b735dc2c5049c3dbfddb33451e62d
Kirito <halid.gundz@gmail.com>
2022-10-10 11:57:34 +0200

    UserEntityCreated

---
+++ src/main/java/com/library/domain/User.java
@@ -0,0 +1,80 @@
+package com.library.domain;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "tbl_user")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long id;
+	
+
+	@Size(min=2,max=30,message="Your Firstname '${validatedValue}' must be between {min} and {max} chars long")
+	@NotNull(message="Please provide your firstname")
+	@Column(length = 30,nullable=false)
+	private String firstName;
+	
+	@Size(min=2,max=30,message="Your Lastname '${validatedValue}' must be between {min} and {max} chars long")
+	@NotNull(message="Please provide your lastname")
+	@Column(length = 30,nullable=false)
+	private String lastName;
+	
+	@Size(min=-2,max=2,message="Score '${validatedValue}' must be between {min} and {max} chars long")
+	@NotNull(message="Please provide score")
+	@Column(nullable=false)
+	private Integer score=0;
+	
+	@Size(min=10,max=100,message="Address '${validatedValue}' must be between {min} and {max} chars long")
+	@NotNull(message="Please provide your address")
+	@Column(length = 100,nullable=false)
+	private String address;
+	
+	@NotNull(message="Please provide your name")
+	@Column(nullable=false)
+	@Pattern(regexp = "^((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$", message = "Please provide valid phone number")
+	private String phone;
+	
+	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy/MM/dd")
+	private Date birthDate;
+	
+
+	@Size(min=10,max=80)
+	@Column(nullable = false/*,unique = true*/)
+	@Email(message="Provide valid email")
+	private String email;
+	
+	private String password;
+	
+	private LocalDateTime createDate;
+	
+	private String resetPasswordCode;
+	
+	private Boolean builtIn;
+	
+	
+}
