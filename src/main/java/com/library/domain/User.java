4280d5051e654aaa709962271361729b667586e7
javafadu <javafadu@gmail.com>
2022-10-10 17:38:42 +0300

    kritov2

--- src/main/java/com/library/domain/User.java
+++ src/main/java/com/library/domain/User.java
@@ -26,60 +26,41 @@ import lombok.Setter;
 @NoArgsConstructor
 
 @Entity
-@Table(name = "tbl_user")
+@Table(name = "tbl_users")
 public class User {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	
 
-	@Size(min=2,max=30,message="Your Firstname '${validatedValue}' must be between {min} and {max} chars long")
-	@NotNull(message="Please provide your firstname")
 	@Column(length = 30,nullable=false)
 	private String firstName;
-	
-	@Size(min=2,max=30,message="Your Lastname '${validatedValue}' must be between {min} and {max} chars long")
-	@NotNull(message="Please provide your lastname")
+
 	@Column(length = 30,nullable=false)
 	private String lastName;
-	
-	@Size(min=-2,max=2,message="Score '${validatedValue}' must be between {min} and {max} chars long")
-	@NotNull(message="Please provide score")
+
 	@Column(nullable=false)
-	private int score=0;
-	
-	@Size(min=10,max=100,message="Address '${validatedValue}' must be between {min} and {max} chars long")
-	@NotNull(message="Please provide your address")
+	private Integer score=0;
+
 	@Column(length = 100,nullable=false)
 	private String address;
-	
-	@NotNull(message="Please provide your name")
+
 	@Column(nullable=false)
-	@Pattern(regexp = "^((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$", message = "Please provide valid phone number")
 	private String phone;
 	
-	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy/MM/dd")
-	@Column(nullable=false)
+	@Column
 	private Date birthDate;
-	
 
-	@Size(min=10,max=80)
-	@Column(nullable = false/*,unique = true*/)
-	@Email(message="Provide valid email")
+	@Column(length = 80, nullable = false, unique = true)
 	private String email;
 	
 	@Column(nullable=false)
-	//TO DO:Hashed password
 	private String password;
 	
-
-	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy/MM/dd HH:mm:ss", timezone= "Turkey")
 	@Column(nullable=false)
 	private LocalDateTime createDate;
 	
-	@Column(nullable=false)
-	//TO DO:Hashed password
+	@Column
 	private String resetPasswordCode;
 	
 	@Column(nullable=false)
