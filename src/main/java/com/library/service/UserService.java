c3e8a1bf95c9d1d883707930116ce991118bde0a
javafadu <javafadu@gmail.com>
2022-10-11 22:54:37 +0300

    v3

---
+++ src/main/java/com/library/service/UserService.java
@@ -0,0 +1,63 @@
+package com.library.service;
+
+import com.library.domain.Loan;
+import com.library.domain.Role;
+import com.library.domain.User;
+import com.library.domain.enums.RoleType;
+import com.library.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+@AllArgsConstructor
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    public void saveUserService() {
+
+
+
+    //    Role role = new Role();
+    //    Set<Role> roles = new HashSet<>();
+    //    role.setName(RoleType.ROLE_ADMIN);
+    //    roles.add(role);
+
+    //    Loan loan = new Loan();
+    //    List<Loan> loans = new ArrayList<>();
+    //
+    //    loan.setNotes("test");
+    //    loans.add(loan);
+
+
+        Date today = Calendar.getInstance().getTime();
+
+        User user = new User();
+
+        user.setFirstName("Ali");
+        user.setLastName("Gel");
+        user.setScore (0);
+        user.setAddress("Adres Adres dfsd");
+        user.setPhone("234232342");
+        user.setBirthDate(today);
+        user.setEmail("test3@test.com");
+        user.setPassword("1234a");
+        user.setCreateDate(LocalDateTime.now());
+        user.setResetPasswordCode("resetpass");
+        // user.setBuiltIn(false);
+        // user.setUserBooks(loans);
+        // user.setRole(roles);
+
+        userRepository.save(user);
+
+    }
+
+
+
+
+}
