4e621ca39eeb547f5c30c99a60f45dd24b738f82
hvicil <100441731+hvicil@users.noreply.github.com>
2022-10-11 22:43:22 +0200

    11102245

---
+++ src/main/java/com/library/service/BookService.java
@@ -0,0 +1,14 @@
+package com.library.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class BookService {
+
+
+
+
+
+}
