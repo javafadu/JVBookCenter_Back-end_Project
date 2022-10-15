6af0d556d798d21291cbff1265679af8afc4a2a3
alkeskn85 <alkeskn85@gmail.com>
2022-10-15 20:07:41 +0200

    ExceptionHandler

---
+++ src/main/java/com/library/exception/message/ErrorMessage.java
@@ -0,0 +1,29 @@
+package com.library.exception.message;
+
+public class ErrorMessage {
+
+
+    public final static String RESOURCE_NOT_FOUND_MESSAGE="Resource with id %d not found";
+
+    public final static String USER_NOT_FOUND_MESSAGE="User with email %s not found";
+
+    public final static String EMAIL_ALREADY_EXIST="Email already exist:%s";
+
+    public final static String ROLE_NOT_FOUND_MESSAGE="Role with name %s not found";
+
+    public final static String NOT_PERMITTED_METHOD_MESSAGE="You dont have any permission to change this value";
+
+    public final static String PASSWORD_NOT_MATCHED="Your password are not matched";
+
+    public final static String IMAGE_NOT_FOUND_MESSAGE="ImageFile with id %s not found";
+
+    public final static String RESERVATION_TIME_INCORRECT_MESSAGE="Reservation pick up time or drop off time not correct";
+
+    public final static String CAR_NOT_AVAILABLE_MESSAGE="Car is not available for selected time";
+
+    public final static String EXCEL_REPORT_CREATION_ERROR_MESSAGE="Error occurred while generation excel report";
+
+    public final static String CAR_USED_BY_RESERVATION_MESSAGE="Car couldn't be deleted. Car is used by a reservation";
+
+    public final static String USER_USED_BY_RESERVATION_MESSAGE="User couldn't be deleted. User is used by a reservation";
+}
