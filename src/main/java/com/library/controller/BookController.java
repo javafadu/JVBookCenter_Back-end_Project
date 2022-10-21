9f47f241f89261ee0d46ef5cd3a2a2eca7a6c312
hvicil <100441731+hvicil@users.noreply.github.com>
2022-10-21 13:23:17 +0200

    21101325

--- src/main/java/com/library/controller/BookController.java
+++ src/main/java/com/library/controller/BookController.java
@@ -23,14 +23,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping()
+@RequestMapping("/books")
 @AllArgsConstructor
 public class BookController {
 
     private BookService bookService;
 
 
-    @PostMapping("/admin/books/add")
+    @PostMapping()
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<BookRegisterResponse> createBook(@Valid @RequestBody BookRegisterRequest bookRegisterRequest)  {
 
@@ -41,7 +41,7 @@ public class BookController {
 
     }
 
-    @GetMapping("/books/{id}")
+    @GetMapping("/{id}")
 
     public ResponseEntity<BookRegisterResponse> getBookById(@PathVariable Long id) {
         BookRegisterResponse bookResponse = bookService.findBookById(id);
@@ -51,7 +51,7 @@ public class BookController {
 
     }
 
-    @GetMapping("/books")
+    @GetMapping()
     public ResponseEntity<Page<BookRegisterResponse>> getBooksWithPage (HttpServletRequest request,
                                                                              @RequestParam(required = false, value = "page", defaultValue = "0") int page,
                                                                              @RequestParam(required = false,value = "size", defaultValue = "5") int size,
@@ -63,7 +63,7 @@ public class BookController {
         return new ResponseEntity<>(bookRegisterResponses, HttpStatus.CREATED);
     }
 
-    @PutMapping("/admin/books/{id}")
+    @PutMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
 
     public ResponseEntity<BookUpdateResponse> updateBook(@PathVariable("id") Long id,
@@ -77,7 +77,7 @@ public class BookController {
     }
 
 
-        @DeleteMapping("/admin/books/{id}")
+        @DeleteMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<BookRegisterResponse> deleteBook(@PathVariable Long id){
         BookRegisterResponse bookResponse = bookService.deleteBookById(id);
