c6a7e238017c3af04f32b9d4d84f6e4aaef3d8cb
javafadu <javafadu@gmail.com>
2022-10-27 00:23:13 +0300

    27Oct-0025

--- src/main/java/com/library/JavanarlarLibraryApplication.java
+++ src/main/java/com/library/JavanarlarLibraryApplication.java
@@ -5,6 +5,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -15,6 +18,17 @@ public class JavanarlarLibraryApplication {
 
         SpringApplication.run(JavanarlarLibraryApplication.class, args);
 
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*").allowedMethods("*");
+                //  WebMvcConfigurer.super.addCorsMappings(registry);
+            }
+        };
     }
 
 
